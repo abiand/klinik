e3a1c1a36e4e68da0165b7e0edc3cd01fef95dcb
abiand <abiand86@gmail.com>
2025-06-11 23:00:48 +0700

    11-06-2025

--- public/assets/js/pages/kepegawaian.init.js
+++ public/assets/js/pages/kepegawaian.init.js
@@ -15,7 +15,42 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     
 });*/
+// FilePond
+FilePond.registerPlugin(
+    // encodes the file as base64 data
+    FilePondPluginFileEncode,
+    // validates the size of the file
+    FilePondPluginFileValidateSize,
+    // corrects mobile image orientation
+    FilePondPluginImageExifOrientation,
+    // previews dropped images
+    FilePondPluginImagePreview
+);
 
+var inputMultipleElements = document.querySelectorAll('input.filepond-input-multiple');
+if(inputMultipleElements){
+
+// loop over input elements
+Array.from(inputMultipleElements).forEach(function (inputElement) {
+    // create a FilePond instance at the input element location
+    FilePond.create(inputElement);
+})
+//asds
+FilePond.create(
+    document.querySelector('.filepond-input-circle'), {
+        labelIdle: 'Drag & Drop your picture or <span class="filepond--label-action">Browse</span>',
+        imagePreviewHeight: 170,
+        imageCropAspectRatio: '1:1',
+        imageResizeTargetWidth: 200,
+        imageResizeTargetHeight: 200,
+        stylePanelLayout: 'compact circle',
+        styleLoadIndicatorPosition: 'center bottom',
+        styleProgressIndicatorPosition: 'right bottom',
+        styleButtonRemoveItemPosition: 'left bottom',
+        styleButtonProcessItemPosition: 'right bottom',
+    }
+);
+}
 $(document).ready(function () {
         var counter = 1;
 
@@ -606,7 +641,7 @@ var attroptions = {
         }
     ]
 };
-
+/*
 var attrList = new List('users', attroptions);
 attrList.add({
     name: 'Leia',
@@ -614,7 +649,7 @@ attrList.add({
     image: 'assets/images/users/avatar-5.jpg',
     id: 5,
     timestamp: '67893'
-});
+}); */
 
 // Existing List
 var existOptionsList = {
@@ -634,4 +669,18 @@ var paginationList = new List('pagination-list', {
     pagination: true
 });
 
+/*
 
+var dropzonePreviewNode = document.querySelector("#dropzone-preview-list");
+dropzonePreviewNode.id = "";
+if(dropzonePreviewNode){
+    var previewTemplate = dropzonePreviewNode.parentNode.innerHTML;
+    dropzonePreviewNode.parentNode.removeChild(dropzonePreviewNode);
+    var dropzone = new Dropzone(".dropzone", {
+        url: 'https://httpbin.org/post',
+        method: "post",
+        previewTemplate: previewTemplate,
+        previewsContainer: "#dropzone-preview",
+    });
+}
+*/
